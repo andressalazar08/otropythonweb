b18266480dda8ab66b221b1dc51c838c53594edc
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2019-11-23 17:46:55 -0500

    primer ajuste

---
+++ py1.py
@@ -0,0 +1,13 @@
+name=input()
+#hay que ponerle una f al string para interpretar una variable
+print(f"hola, como estás {name}!")
+
+x=34
+
+if x>0:
+    print("x es positivo")
+elif x<0:
+    print("x es negativo")
+else:
+    print("x es cero")
+    
